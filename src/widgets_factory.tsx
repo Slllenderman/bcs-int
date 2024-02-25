94c4015c37f697e58e9c4ce045db65ecba381115
Khar1tonov-Andrey <ander123345454@mail.ru>
2024-02-25 22:53:05 +0300

    theming

--- src/widgets_factory.tsx
+++ src/widgets_factory.tsx
@@ -5,7 +5,10 @@ import { Widget, WidgetTheme } from './widget';
 export type FCWC<Props> = FC<PropsWithChildren<Props>>;
 
 export interface WidgetsFactory {
+    
     setTheme(theme: WidgetTheme): void;
+    getTheme(): WidgetTheme;
+
     getNavbar(): FCWC<props.NavbarProps>;
     getConferenceShowcase(): FC<props.ConferenceShowcaseProps>;
     getConferenceDateWidget(): FC<props.ConferenceDateProps>;
