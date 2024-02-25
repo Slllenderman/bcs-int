0257f435e735b757f921eaf02785da5c28159019
Khar1tonov-Andrey <ander123345454@mail.ru>
2024-02-25 20:34:10 +0300

    add_theme

--- src/widgets_factory.tsx
+++ src/widgets_factory.tsx
@@ -1,10 +1,11 @@
 import { FC, PropsWithChildren } from 'react'
 import * as props from "./widgets_props"
-import { Widget } from './widget';
+import { Widget, WidgetTheme } from './widget';
 
 export type FCWC<Props> = FC<PropsWithChildren<Props>>;
 
 export interface WidgetsFactory {
+    setTheme(theme: WidgetTheme): void;
     getNavbar(): FCWC<props.NavbarProps>;
     getConferenceShowcase(): FC<props.ConferenceShowcaseProps>;
     getConferenceDateWidget(): FC<props.ConferenceDateProps>;
