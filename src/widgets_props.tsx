e1fdacc4f566da501992dba3efd3364526093dfb
Vlad-prog-byte <vlad2mickevich@gmail.com>
2024-04-25 21:35:09 +0300

    add heading and footer

--- src/widgets_props.tsx
+++ src/widgets_props.tsx
@@ -10,14 +10,6 @@ export interface NavbarProps extends Widget {
     logo_img: String;
 }
 
-// Conference showcase ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// https://www.figma.com/file/ySvtPDNcNYmCHJlhLACAFi/%F0%9F%A7%A0-%D0%A1%D0%B0%D0%B9%D1%82-%D0%98%D0%98%D0%90%D0%A1%D0%A3?type=design&node-id=207-1569&mode=design&t=DqHHb2fXLk5DJBlu-4
-
-export interface ConferenceShowcaseProps extends Widget {
-    title: String;
-    subtitle: String;
-}
-
 // Conference date & format ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // https://www.figma.com/file/ySvtPDNcNYmCHJlhLACAFi/%F0%9F%A7%A0-%D0%A1%D0%B0%D0%B9%D1%82-%D0%98%D0%98%D0%90%D0%A1%D0%A3?type=design&node-id=207-1565&mode=design&t=9zceGAlzsGn9zRcY-4
 
@@ -46,4 +38,21 @@ export interface ConferenceInfoItemProps extends Widget {
 export interface ConferenceLoactionProps extends Widget {
     address: String;
     ya_link: String;
+}
+
+// Conference location ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// https://www.figma.com/file/ySvtPDNcNYmCHJlhLACAFi/%F0%9F%A7%A0-%D0%A1%D0%B0%D0%B9%D1%82-%D0%98%D0%98%D0%90%D0%A1%D0%A3?type=design&node-id=132-3155&mode=design&t=V3ANFZha7n2A741F-4
+export interface ConferenceHeadingProps extends Widget {
+    image: String;
+    title: String;
+    subtitle: String;
+    part_href: String;
+}
+
+// Conference footer ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// https://www.figma.com/file/ySvtPDNcNYmCHJlhLACAFi/%F0%9F%A7%A0-%D0%A1%D0%B0%D0%B9%D1%82-%D0%98%D0%98%D0%90%D0%A1%D0%A3?type=design&node-id=207-3386&mode=design&t=l72HAW8gkCmw9N66-4
+export interface ConferenceFooter extends Widget {
+    connection: [String];
+    start_date: Date;
+    end_date: Date;
 }
