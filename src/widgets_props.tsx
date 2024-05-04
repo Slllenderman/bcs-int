98df1e59848c991c1437141cdc07f85bf7136563
Khar1tonov-Andrey <ander123345454@mail.ru>
2024-05-04 18:42:43 +0300

    add_description

--- src/widgets_props.tsx
+++ src/widgets_props.tsx
@@ -4,10 +4,10 @@ import { Widget } from "./widget";
 // https://www.figma.com/file/ySvtPDNcNYmCHJlhLACAFi/%F0%9F%A7%A0-%D0%A1%D0%B0%D0%B9%D1%82-%D0%98%D0%98%D0%90%D0%A1%D0%A3?type=design&node-id=207-1610&mode=design&t=DqHHb2fXLk5DJBlu-4
 
 export interface NavbarProps extends Widget {
-    auth_href: String;
-    home_href: String;
-    logo_name: String;
-    logo_img: String;
+    auth_href: string;
+    home_href: string;
+    logo_name: string;
+    logo_img: string;
 }
 
 // Conference date & format ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -28,30 +28,30 @@ export interface ConferenceDateProps extends Widget {
 // No link 
 
 export interface ConferenceInfoItemProps extends Widget {
-    title: String;
-    info: String;
+    title: string;
+    info: string;
 }
 
 // Conference location ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // https://www.figma.com/file/ySvtPDNcNYmCHJlhLACAFi/%F0%9F%A7%A0-%D0%A1%D0%B0%D0%B9%D1%82-%D0%98%D0%98%D0%90%D0%A1%D0%A3?type=design&node-id=247-3111&mode=design&t=9zceGAlzsGn9zRcY-4
 
 export interface ConferenceLoactionProps extends Widget {
-    address: String;
-    ya_link: String;
+    address: string;
+    ya_link: string;
 }
 
 // Conference location ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // https://www.figma.com/file/ySvtPDNcNYmCHJlhLACAFi/%F0%9F%A7%A0-%D0%A1%D0%B0%D0%B9%D1%82-%D0%98%D0%98%D0%90%D0%A1%D0%A3?type=design&node-id=132-3155&mode=design&t=V3ANFZha7n2A741F-4
 export interface ConferenceHeadingProps extends Widget {
-    title: String;
-    subtitle: String;
-    part_href: String;
+    title: string;
+    subtitle: string;
+    part_href: string;
 }
 
 // Conference footer ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // https://www.figma.com/file/ySvtPDNcNYmCHJlhLACAFi/%F0%9F%A7%A0-%D0%A1%D0%B0%D0%B9%D1%82-%D0%98%D0%98%D0%90%D0%A1%D0%A3?type=design&node-id=207-3386&mode=design&t=l72HAW8gkCmw9N66-4
 export interface ConferenceFooter extends Widget {
-    connection: String[];
+    connection: string[];
     start_date: Date;
     end_date: Date;
 }
@@ -59,10 +59,10 @@ export interface ConferenceFooter extends Widget {
 // Conference userCard ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // https://www.figma.com/file/ySvtPDNcNYmCHJlhLACAFi/%F0%9F%A7%A0-%D0%A1%D0%B0%D0%B9%D1%82-%D0%98%D0%98%D0%90%D0%A1%D0%A3?type=design&node-id=207-1625&mode=design&t=tZ260xfcOh6uD8P1-4
 export interface ConferenceUserCard extends Widget {
-    post: String;
-    image: String;
-    initials: String;
-    info: String;
+    post: string;
+    image: string;
+    initials: string;
+    info: string;
 }
 
 // Conference logo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -72,4 +72,13 @@ export interface ConferenceLogo extends Widget {
     img_src: string;
     title: string;
     variant: string;
+}
+
+// Conference description
+// https://www.figma.com/file/ySvtPDNcNYmCHJlhLACAFi/%F0%9F%A7%A0-%D0%A1%D0%B0%D0%B9%D1%82-%D0%98%D0%98%D0%90%D0%A1%D0%A3?type=design&node-id=207-1477&mode=design&t=UeMZnC2nIjhPJOEn-4
+export interface ConferenceDescription extends Widget {
+    about_text: string;
+    purpose_text: string;
+    topic_text: string;
+    participation_text: string;
 }
