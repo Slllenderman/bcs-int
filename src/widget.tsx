a8bc9c73ddb4414a66142a134c6477d91d856175
Khar1tonov-Andrey <ander123345454@mail.ru>
2024-05-21 08:32:00 +0300

    delete className

--- src/widget.tsx
+++ src/widget.tsx
@@ -1,11 +1,10 @@
-import { createContext, useContext } from "react";
+
 
 export class Widget {
     width?: number = 1;
     height?: number = 1;
     font_size?: number = 1;
     visibility?: Boolean = true;
-    className?: string = "";
     theme?: WidgetTheme;
 }
 
@@ -16,7 +15,6 @@ export const useWidget = (props: any): any => {
     if(typeof ret.height === 'undefined') ret.height = defWidget.height
     if(typeof ret.font_size === 'undefined') ret.font_size = defWidget.font_size
     if(typeof ret.visibility === 'undefined') ret.visibility = defWidget.visibility
-    if(typeof ret.className === 'undefined') ret.className = defWidget.className
     return ret
 }
 
@@ -35,21 +33,3 @@ export class WidgetTheme {
     second_typography: WidgetTypography = new WidgetTypography();
     third_typography: WidgetTypography = new WidgetTypography();
 }
-
-
-const ThemeContext = createContext({})
-
-export const useTheme = (props: any): any => {
-    const { theme, setTheme }: any = useContext(ThemeContext)
-    if(typeof props.theme !== 'undefined'){
-        let new_theme = theme
-        if(typeof props.theme.first_basecolor !== 'undefined')
-            new_theme.palette.primary.main = props.theme.first_basecolor
-        if(typeof props.theme.second_basecolor !== 'undefined')
-            new_theme.palette.primary.dark = props.theme.second_basecolor
-        if(typeof props.theme.third_basecolor !== 'undefined')
-            new_theme.palette.primary.light = props.theme.third_basecolor
-        setTheme(new_theme)
-    }
-    return theme
-} 
